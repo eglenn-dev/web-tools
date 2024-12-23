05cb39ce46b4333858a3839df5308d108ac85d72
Ethan Glenn <ethan+git@eglenn.dev>
2024-12-22 22:11:57 -0700

    Update footer links to direct to personal portfolio; remove outdated links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,14 +4,8 @@ export default function Footer() {
             <div className="container mx-auto px-4 text-center">
                 <p>&copy; {new Date().getFullYear()} WebTools. All rights reserved.</p>
                 <div className="mt-4">
-                    <a href="#" className="text-gray-400 hover:text-white mx-2">
-                        Privacy Policy
-                    </a>
-                    <a href="#" className="text-gray-400 hover:text-white mx-2">
-                        Terms of Service
-                    </a>
-                    <a href="#" className="text-gray-400 hover:text-white mx-2">
-                        Contact Us
+                    <a href="https://eglenn.dev" className="text-gray-400 hover:text-white mx-2">
+                        Tools Developed by Ethan Glenn
                     </a>
                 </div>
             </div>
